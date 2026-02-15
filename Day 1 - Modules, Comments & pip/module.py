6e5c384d70b5a567c1e516c6d98b49873e42b476
yagnik0 <yashyagnik456@gmail.com>
2026-02-15 07:13:38 +0530

    second commit

--- Day 1 - Modules, Comments & pip/module.py
+++ Day 1 - Modules, Comments & pip/module.py
@@ -2,3 +2,7 @@ import pyjokes
 
 joke = pyjokes.get_joke()
 print(joke)
+
+
+# REPL :- Read Evaluate Print loop
+
---
+++ Day 1 - Modules, Comments & pip/practiseset1.py
@@ -0,0 +1,20 @@
+#Q.1:- Write a program to print a nursery poem in python.
+
+# print("Twinkle, twinkle, little star, how I wonder what you are. Up above the world so high like a diamond in the sky. Twinkle, twinkle, little star, how I wonder what you are.When the blazing sun is set, and the grass with dew is wet. Then you show your littlelight, twinkle, twinkle all the night. Twinkle, twinkle little star, how I wonder what you are.Then the traveler in the dark thanks you for your tiny spark. How could he see where togo if you did not twinkle so? Twinkle, twinkle little star, how I wonder what you are.As your bright and tiny spark lights the traveler in the dark, though I know not what youare, twinkle, twinkle, little star. Twinkle, twinkle, little star, how I wonder what you are.")
+
+# Q.2:- Install an external module and use it to perform an operation of your intrest.
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("jai bhootnath")
+# engine.runAndWait()
+
+# Q.3 :- Write a python program to print the contents of a directory using the os module. Search online for the function which does that.
+
+# import os
+# print("Contents of the directory are:", os.listdir())
+
+
+
+
+
