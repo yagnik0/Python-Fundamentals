11715036202f057f4ca3e1936f1587d810c47dff
yagnik0 <yashyagnik456@gmail.com>
2026-02-28 19:21:22 +0530

    fourth commit

---
+++ Day 4 - Lists & Tuples/list&tuples.py
@@ -0,0 +1,43 @@
+friends = ["Apple", "Orange", 34.78, 5, False, "Aakash", "Yash"]
+
+# print(friends[0])
+
+friends[0] = "Mango"
+
+# Lists are mutable
+
+# print(friends[0])
+
+# lists Methods 🐍
+
+l1 = [1,56,67,23, 90,34, 4, 7]
+# l1.sort()
+# l1.reverse()
+# l1.append("Krishna")
+# l1.insert(5,77)
+# l1.pop(4)
+l1.remove(34)
+# print(l1)
+
+
+# Tuples 🐍 :- are immutable datatype
+
+a  = (1,2,6,5,1)
+
+# print(type(a))
+
+# Methods of tuples
+
+counts = a.count(1)
+print(counts)
+
+index = a.index(5)
+print(index)
+
+
+
+
+
+
+
+
---
+++ Day 4 - Lists & Tuples/practiseset4.py
@@ -0,0 +1,68 @@
+# Q.1 :- Write a program to store seven fruits in a list entered by the user.
+
+fruitsName = ["Mango", "Orange", "Grapes", "Apple", "Kiwi", "Papaya", "Banana"]
+
+# print(fruitsName)
+
+# Q.2 :- Write a program to accept marks of 6 students and display them in a sorted manner
+
+# marksObtained =  []
+
+# firstMarks = int(input("Enter the marks of student 1:- "))
+# marksObtained.append(firstMarks)
+
+# secondMarks = int(input("Enter the marks of student 2:- "))
+# marksObtained.append(secondMarks)
+
+# thirdMarks = int(input("Enter the marks of student 3:- "))
+# marksObtained.append(thirdMarks)
+
+# fourthMarks = int(input("Enter the marks of student 4:- "))
+# marksObtained.append(fourthMarks)
+
+# fifthMarks = int(input("Enter the marks of student 5:- "))
+# marksObtained.append(fifthMarks)
+
+# sixthMarks = int(input("Enter the marks of student 6:- "))
+# marksObtained.append(sixthMarks)
+
+
+# print(marksObtained)
+
+# marksObtained.sort()
+
+# print(marksObtained)
+
+# Q.3 :- Check that a tuple type cannot be changed in python.
+
+# a = (1, 2, 3, 4, 5)
+
+# a[1] = 2
+
+# print(a)
+
+# Q.4:- Write a program to sum a list with 4 numbers
+
+numlist = [1,2,3,4]
+
+# sum = numlist[0] + numlist[1] + numlist[2] + numlist[3]
+
+# print(sum)
+
+# 2nd method
+
+total = sum(numlist)
+
+print(total)
+
+# Q.5 :- Write a program to count the number of zeros in the following tuple:
+
+a = (7, 0, 8, 0, 0, 9)
+
+countZero = a.count(0)
+
+print(countZero)
+
+
+
+
