717bf88b53125e5c4204d519ea58aa09badfa606
yagnik0 <yashyagnik456@gmail.com>
2026-03-03 12:34:52 +0530

    seventh commit

---
+++ Day 7 - Loops/loops.py
@@ -0,0 +1,93 @@
+# Printing a number from 1 to 6
+
+# for i in range(1,6):
+  # print(i)
+
+# There are two types of loops in python
+# 1:- while loop
+# 2:- for loop
+
+# while loop:- 🐍
+
+# i = 1
+
+# while(i<6):
+#   print(i)
+#   i+=1
+
+# Write a program to print 1 to 50 using a while loop.
+
+# i = 1
+# while(i<51):
+#   print(i)
+#   i+=1
+
+# Write a program to print the content of a list using while loops.
+
+l = ["Yash", "Mahadev", "Krishna", "Bramharshi"]
+
+# print(len(l))
+
+# i = 0
+
+# while( i < len(l)):
+#   print(l[i])
+#   i+=1
+
+# For loop:- 🐍
+
+# for looop with list:-
+# for item in l:
+  # print(item)
+
+# for loop with tuples:-
+
+# t = (6, 2335, 78, 122)
+# for i in t:
+#   print(i)
+
+# for loop with strings:-
+
+# s = "krishna"
+
+# for i in s:
+#   print(i)
+
+# for loop with else
+
+# l1  = [3, 5, 8]
+
+# for i in l1:
+#   print(i)
+
+# else:
+#   print("done")
+
+# for loop with i and break 
+
+# for i in range (100):
+#   if(i== 35):
+#     break   # exit the loop right now
+#   print(i)
+
+# for loop with i and continue
+
+# for i in range (100):
+#   if(i== 35):
+#     continue # skip this iteration right now
+#   print(i)
+
+# for loop with pass statement
+
+
+l2 = [34,78,90]
+
+for i in l2:
+  pass
+
+
+
+
+
+
+
